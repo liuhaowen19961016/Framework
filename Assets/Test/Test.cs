b0d174e4b56bea1279c37cb4680dbd29ff0d4eca
DESKTOP-6SLRMLB\kaboom <597094538@qq.com>
2023-12-04 16:08:00 +0800

    Test

--- Assets/Test/Test.cs
+++ Assets/Test/Test.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,24 @@ public class Test : MonoBehaviour
 {
     void Start()
     {
+        var v1 = GameUtils.GetRandomEmum<ETestV>();
+        //Debug.LogFormat()
 
+        //Log.Info("info");
+        //Log.Warning("Warning");
+        //Log.Error("Error");
+        //Log.Info(ETestV);
+        //Log.Warning(ETestV);
+        //Log.Error(ETestV);
+
+        TestTest t = new TestTest();
+
+        //Debug.Log(Log.GetString(t));
+        //Debug.Log(Log.GetString(t));
+
+        Log.DebugFormat("ssdd{0} - {1}", 111, 123);
+        Log.Error(null);
+        Log.Info(t);
     }
 
     void Update()
@@ -18,4 +36,20 @@ public class Test : MonoBehaviour
     {
 
     }
+}
+
+public class TestTest : IFormattable
+{
+    public string ToString(string format, IFormatProvider formatProvider)
+    {
+        return "asdasd{0}";
+    }
+}
+
+public enum ETestV
+{
+    T1,
+    T2,
+    T3,
+    T4,
 }
