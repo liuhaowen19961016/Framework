81b956d2b7e4e2e6ede4b164740a9666dd431c8c
刘浩文 <597094538@qq.com>
2024-06-16 20:34:55 +0800

    整理

--- Assets/Scripts/Hotfix/Framework/Module/Event/EventBase.cs
+++ Assets/Scripts/Hotfix/Framework/Module/Event/EventBase.cs
@@ -1,18 +1,5 @@
 namespace Framework
 {
-    public enum EEventType
-    {
-        Invalid,
-
-        #region UI
-
-        AddFlyObjData, //添加飞物体
-        FlyObjGroupComplete, //完成本组飞物体
-        FlyObjSingleComplete, //完成单个飞物体
-
-        #endregion UI
-    }
-
     public class EventBase : IReferencePoolObject
     {
         public EEventType EventType;
---
+++ Assets/Scripts/Hotfix/Framework/Module/Event/EventType.cs
@@ -0,0 +1,15 @@
+namespace Framework
+{
+    public enum EEventType
+    {
+        Invalid,
+
+        #region UI
+
+        AddFlyObjData, //添加飞物体
+        FlyObjGroupComplete, //完成本组飞物体
+        FlyObjSingleComplete, //完成单个飞物体
+
+        #endregion UI
+    }
+}
