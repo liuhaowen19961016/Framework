802d5d54128f9dab1ce4f1e9cd65bad1f2165bbf
haowenliudragonplus <haowen.liu@dragonplus.com>
2024-07-07 15:50:39 +0800

    动画相关

--- Assets/Scripts/Main/Framework/MonoScript/EmptyAnimationEvent.cs
+++ Assets/Scripts/Main/Framework/MonoScript/EmptyAnimationEvent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 为了获取Animation上的Event关键帧时不报错
+/// 为了获取Animation上的空Event关键帧时不报错
 /// </summary>
 public class EmptyAnimationEvent : MonoBehaviour
 {
