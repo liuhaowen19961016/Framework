c5b4a9db31fb87676b4ff265b87c55068294cc2b
haowenliudragonplus <haowen.liu@dragonplus.com>
2024-08-01 10:06:11 +0800

    测试

---
+++ Assets/Scripts/Hotfix/后续通过工具生成的/UISubviewTemp.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISubviewTemp
+{
+    public static Dictionary<int, UISubViewConfig> UISubViewConfigs = new Dictionary<int, UISubViewConfig>()
+    {
+        { 1, new UISubViewConfig() { Id = 1, Path = "xxx" } },
+    };
+}
