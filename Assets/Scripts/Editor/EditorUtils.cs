5dad78bc8f40dd23f182cdb4e0c13a9f20c676eb
刘浩文 <597094538@qq.com>
2024-08-03 15:27:24 +0800

    UI相关

--- Assets/Scripts/Editor/EditorUtils.cs
+++ Assets/Scripts/Editor/EditorUtils.cs
@@ -2,6 +2,9 @@ using System;
 
 public static class EditorUtils
 {
+    /// <summary>
+    /// 显示对话框
+    /// </summary>
     public static void ShowDialogWindow(string title, string content, string btn1Name,
         string btn2Name = "", string btn3Name = "", Action onBtn1 = null, Action onBtn2 = null, Action onBtn3 = null)
     {
