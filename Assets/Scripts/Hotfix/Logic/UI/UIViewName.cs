b8c7ad185041e6fc1ceef09d5ef6e887992f769b
刘浩文 <597094538@qq.com>
2024-08-04 21:45:15 +0800

    UI相关

---
+++ Assets/Scripts/Hotfix/Logic/UI/UIViewName.cs
@@ -0,0 +1,13 @@
+/******************************/
+/*****自动生成的UIViewName代码，禁止手动修改*****/
+/*****生成时间：2024-8-4 21:44:32*****/
+/*****************************/
+
+public class UIViewName
+{
+	public const int UIView_Main = 1;
+
+	public const int UIView_Shop = 2;
+
+
+}
