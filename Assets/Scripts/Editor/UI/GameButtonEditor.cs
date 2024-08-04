6a25ab4dc4264a37295f15d10a3fc7dd1582c6a4
刘浩文 <597094538@qq.com>
2024-08-04 09:42:05 +0800

    GameButton

--- Assets/Scripts/Editor/UI/GameButtonEditor.cs
+++ Assets/Scripts/Editor/UI/GameButtonEditor.cs
@@ -9,12 +9,14 @@ public class GameButtonEditor : SelectableEditor
 {
     private SerializedProperty m_EnableClickSound;
     private SerializedProperty m_ClickSoundId;
+    private SerializedProperty m_EnableClickAni;
 
     protected override void OnEnable()
     {
         base.OnEnable();
         m_EnableClickSound = serializedObject.FindProperty("enableClickSound");
         m_ClickSoundId = serializedObject.FindProperty("clickSoundId");
+        m_EnableClickAni = serializedObject.FindProperty("enableClickAni");
     }
 
     public override void OnInspectorGUI()
@@ -26,6 +28,7 @@ public class GameButtonEditor : SelectableEditor
         serializedObject.Update();
         EditorGUILayout.PropertyField(m_EnableClickSound, new GUIContent("开启点击按钮音效"));
         EditorGUILayout.PropertyField(m_ClickSoundId, new GUIContent("按钮音效id"));
+        EditorGUILayout.PropertyField(m_EnableClickAni, new GUIContent("开启点击按钮动画"));
         serializedObject.ApplyModifiedProperties();
     }
 }
--- Assets/Scripts/Main/Framework/Module/UI/Component/GameButton.cs
+++ Assets/Scripts/Main/Framework/Module/UI/Component/GameButton.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
 namespace UnityEngine.UI
 {
@@ -11,9 +12,24 @@ namespace UnityEngine.UI
         [SerializeField]
         private int clickSoundId; //点击按钮时的音效
 
+        [SerializeField]
+        private bool enableClickAni = true;
+
+        private Sequence clickAniSeq;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            clickAniSeq.Kill(true);
+        }
+
         public override void OnPointerClick(PointerEventData eventData)
         {
             base.OnPointerClick(eventData);
+            if (enableClickAni)
+            {
+                PlayAni();
+            }
             if (enableClickSound)
             {
                 PlayClickSound();
@@ -24,6 +40,19 @@ namespace UnityEngine.UI
         {
             //todo logic 播放声音
             Debug.LogError("播放声音");
+        }
+
+        private void PlayAni()
+        {
+            clickAniSeq = DOTween.Sequence();
+            clickAniSeq.Append(transform.DOScale(Vector3.one * 0.85f, 0.1f));
+            clickAniSeq.Append(transform.DOScale(Vector3.one * 1f, 0.1f));
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            clickAniSeq.Kill(true);
         }
     }
 }
