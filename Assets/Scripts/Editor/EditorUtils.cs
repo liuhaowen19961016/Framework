6e8a3e2a40aef18c80e9e4d338c86a06dc2843e3
刘浩文 <597094538@qq.com>
2024-08-11 22:08:16 +0800

    优化生成UI工具

--- Assets/Scripts/Editor/EditorUtils.cs
+++ Assets/Scripts/Editor/EditorUtils.cs
@@ -6,10 +6,9 @@ public static class EditorUtils
     /// <summary>
     /// 显示对话框
     /// </summary>
-    public static void ShowDialogWindow(string title, string content, string btn1Name,
-        string btn2Name = "", string btn3Name = "", Action onBtn1 = null, Action onBtn2 = null, Action onBtn3 = null)
+    public static void ShowDialogWindow(string title, string content, string btn1Name)
     {
-        CommonDialogWindow.CreateWindow(title, content, btn1Name, btn2Name, btn3Name, onBtn1, onBtn2, onBtn3);
+        CommonDialogWindow.CreateWindow(title, content, btn1Name);
     }
 
     /// <summary>
@@ -26,14 +25,13 @@ public static class EditorUtils
         }
         return assetPaths;
     }
-    
+
     /// <summary>
     /// 获取当前时间
     /// </summary>
-    /// <returns></returns>
     public static string GenCurDateTimeStr()
     {
-        string dateTimeStr = DateTime.Now.ToString("yyyy-M-d H:m:s");
+        string dateTimeStr = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
         return dateTimeStr;
     }
 }
