6ba24a72c20ce14b18bb9aedb3a60a27370f8390
haowenliudragonplus <haowen.liu@dragonplus.com>
2024-08-30 13:46:35 +0800

    UI

--- Assets/Scripts/Hotfix/测试/UIView_Activity.cs
+++ Assets/Scripts/Hotfix/测试/UIView_Activity.cs
@@ -1,7 +1,35 @@
 using Framework;
+using Hotfix;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class UIView_Activity : UIView_ActivityBase
 {
-    
+    protected override void OnInit(object viewData)
+    {
+        base.OnInit(viewData);
+        viewAniType = EViewAniType.Animator;
+        Debug.LogError("UIView_Main OnInit --" + viewData);
+    }
+
+    protected override void RegisterUIEvent()
+    {
+        base.RegisterUIEvent();
+UIBtn_Activity1.onClick.AddListener(() =>
+{
+    //AddUISubview<>()
+});
+    }
+
+    protected override void OnRefresh()
+    {
+        base.OnRefresh();
+        Debug.LogError("UIView_Main OnRefresh --" + ViewData);
+    }
+
+    protected override void OnOpen()
+    {
+        base.OnOpen();
+        Debug.LogError("UIView_Main OnShow --" + ViewData);
+    }
 }
