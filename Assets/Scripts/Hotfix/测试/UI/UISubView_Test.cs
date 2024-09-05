07bff18435bce7ac2e4f41ab950c613d0a4ac3f9
haowenliudragonplus <haowen.liu@dragonplus.com>
2024-09-05 16:56:45 +0800

    UI

--- Assets/Scripts/Hotfix/测试/UI/UISubView_Test.cs
+++ Assets/Scripts/Hotfix/测试/UI/UISubView_Test.cs
@@ -4,15 +4,5 @@ using UnityEngine.UI;
 
 public class UISubView_Test : UISubView_TestBase
 {
-    protected override void OnShow()
-    {
-        base.OnShow();
-        Debug.LogError("OnShow UISubView_Test");
-    }
-    
-    protected override void OnOpen()
-    {
-        base.OnOpen();
-        Debug.LogError("OnOpen UISubView_Test");
-    }
+
 }
